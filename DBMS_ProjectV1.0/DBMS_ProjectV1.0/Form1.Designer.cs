4b94a2f6c7e9a64992ababce783b9c29a24f2f6f
HMZ_Rajput <127087223+HMZ-rajput@users.noreply.github.com>
2023-06-01 06:56:43 -0700

    Testing

--- DBMS_ProjectV1.0/DBMS_ProjectV1.0/Form1.Designer.cs
+++ DBMS_ProjectV1.0/DBMS_ProjectV1.0/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(307, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Test";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }

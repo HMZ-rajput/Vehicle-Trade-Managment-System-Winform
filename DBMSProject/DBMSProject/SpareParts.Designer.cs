e8653f9d7502ea797b3106f83a61925c199abed6
mzainxo <120658271+mzainxo@users.noreply.github.com>
2023-06-05 19:32:56 +0500

    SpareParts form (READ)

--- DBMSProject/DBMSProject/SpareParts.Designer.cs
+++ DBMSProject/DBMSProject/SpareParts.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Namelbl = new System.Windows.Forms.Label();
+            this.costlbl = new System.Windows.Forms.Label();
+            this.desclbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(467, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Spare Parts";
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(126, 356);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(100, 28);
+            this.deleteBtn.TabIndex = 14;
+            this.deleteBtn.Text = "Delete Part";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(126, 304);
+            this.updateBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(100, 28);
+            this.updateBtn.TabIndex = 13;
+            this.updateBtn.Text = "Update Part";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(126, 254);
+            this.addBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(100, 28);
+            this.addBtn.TabIndex = 12;
+            this.addBtn.Text = "Add Part";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(390, 88);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(580, 374);
+            this.dataGridView.TabIndex = 15;
+            // 
+            // Namelbl
+            // 
+            this.Namelbl.AutoSize = true;
+            this.Namelbl.Location = new System.Drawing.Point(12, 91);
+            this.Namelbl.Name = "Namelbl";
+            this.Namelbl.Size = new System.Drawing.Size(77, 16);
+            this.Namelbl.TabIndex = 16;
+            this.Namelbl.Text = "Part Name: ";
+            // 
+            // costlbl
+            // 
+            this.costlbl.AutoSize = true;
+            this.costlbl.Location = new System.Drawing.Point(12, 175);
+            this.costlbl.Name = "costlbl";
+            this.costlbl.Size = new System.Drawing.Size(67, 16);
+            this.costlbl.TabIndex = 17;
+            this.costlbl.Text = "Part Cost: ";
+            // 
+            // desclbl
+            // 
+            this.desclbl.AutoSize = true;
+            this.desclbl.Location = new System.Drawing.Point(12, 133);
+            this.desclbl.Name = "desclbl";
+            this.desclbl.Size = new System.Drawing.Size(108, 16);
+            this.desclbl.TabIndex = 18;
+            this.desclbl.Text = "Part Description: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 22);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "Enter Part Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 22);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.Text = "Enter Part Description";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(170, 175);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(178, 22);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "Enter Part Cost";
+            // 
+            // SpareParts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(997, 510);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.desclbl);
+            this.Controls.Add(this.costlbl);
+            this.Controls.Add(this.Namelbl);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "SpareParts";
             this.Text = "SpareParts";
+            this.Load += new System.EventHandler(this.SpareParts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label Namelbl;
+        private System.Windows.Forms.Label costlbl;
+        private System.Windows.Forms.Label desclbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- DBMSProject/DBMSProject/SpareParts.cs
+++ DBMSProject/DBMSProject/SpareParts.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,28 @@ namespace DBMSProject
         public SpareParts()
         {
             InitializeComponent();
+        }
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+
+        private void SpareParts_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(localdb)\local;Initial Catalog=VehicleTrade;Integrated Security=True");
+            getSparePartsRecord();
+        }
+        private void getSparePartsRecord()
+        {
+            con.Open();
+            cmd = new SqlCommand("readSparePart", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            this.dataGridView.DataSource = dt;
+            con.Close();
         }
     }
 }

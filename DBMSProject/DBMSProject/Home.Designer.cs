02cfe46005f046d66e4ed067ec1352d95dd8c105
M-Askari01 <111376912+M-Askari01@users.noreply.github.com>
2023-06-06 14:31:26 +0500

    askari on fire

--- DBMSProject/DBMSProject/Home.Designer.cs
+++ DBMSProject/DBMSProject/Home.Designer.cs
@@ -43,9 +43,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(374, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(102, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "Home";
             // 
